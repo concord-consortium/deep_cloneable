16e51247b31e274ea8f0133e879e9ad4a1a90b7e
Noah Paessel <knowuh@gmail.com>
2011-04-08 10:18:26 -0400

    Rails 2.3.11 w/ Bunlder was not modifying AR#clone

--- lib/deep_cloneable.rb
+++ lib/deep_cloneable.rb
@@ -1,45 +1,44 @@
-class ActiveRecord::Base
   module DeepCloneable
-    # clones an ActiveRecord model. 
+    # clones an ActiveRecord model.
     # if passed the :include option, it will deep clone the given associations
     # if passed the :except option, it won't clone the given attributes
     #
     # === Usage:
-    # 
+    #
     # ==== Cloning one single association
     #    pirate.clone :include => :mateys
-    # 
+    #
     # ==== Cloning multiple associations
     #    pirate.clone :include => [:mateys, :treasures]
-    # 
+    #
     # ==== Cloning really deep
     #    pirate.clone :include => {:treasures => :gold_pieces}
-    # 
+    #
     # ==== Cloning really deep with multiple associations
     #    pirate.clone :include => [:mateys, {:treasures => :gold_pieces}]
     #
     # ==== Cloning really deep with multiple associations and a dictionary
     #
-    # A dictionary ensures that models are not cloned multiple times when it is associated to nested models. 
+    # A dictionary ensures that models are not cloned multiple times when it is associated to nested models.
     # When using a dictionary, ensure recurring associations are cloned first:
     #
-    #    pirate.clone :include => [:mateys, {:treasures => [:matey, :gold_pieces], :use_dictionary => true }]    
+    #    pirate.clone :include => [:mateys, {:treasures => [:matey, :gold_pieces], :use_dictionary => true }]
     #
     # If this is not an option for you, it is also possible to populate the dictionary manually in advance:
     #
     #    dict = { :mateys => {} }
     #    pirate.mateys.each{|m| dict[:mateys][m] = m.clone }
-    #    pirate.clone :include => [:mateys, {:treasures => [:matey, :gold_pieces], :dictionary => dict }]    
-    #   
+    #    pirate.clone :include => [:mateys, {:treasures => [:matey, :gold_pieces], :dictionary => dict }]
+    #
     # ==== Cloning a model without an attribute
     #    pirate.clone :except => :name
-    #  
+    #
     # ==== Cloning a model without multiple attributes
     #    pirate.clone :except => [:name, :nick_name]
-    #    
-    # ==== Cloning a model without an attribute or nested multiple attributes   
+    #
+    # ==== Cloning a model without an attribute or nested multiple attributes
     #    pirate.clone :include => :parrot, :except => [:name, { :parrot => [:name] }]
-    # 
+    #
     #
     # ==== cloneable_assocation ActiveRecord syntax sugar:
     #   class Bottle < ActiveRecord::Base
@@ -49,12 +48,12 @@ class ActiveRecord::Base
     #     # drinkers will always be included with clone()
     #     cloneable_associations :drinkers
     #   end
-    
+
     def self.included(base)
-      base.send :include, DeepCloneable::InstanceMethods
+      base.alias_method_chain :clone, :deep
       base.extend DeepCloneable::ClassMethods
     end
-    
+
     module ClassMethods
       # in a AR model:
       #  cloneable_associations :treasure, :shipmates
@@ -66,8 +65,6 @@ class ActiveRecord::Base
       end
     end
 
-    module InstanceMethods
-      
       # check for included associations.
       def include_cloneable_associations(options)
         begin
@@ -84,18 +81,18 @@ class ActiveRecord::Base
           options[:include] += new_assocs
         end
       end
-      
-      def clone(options = {})
+
+      def clone_with_deep(options = {})
         include_cloneable_associations(options)
         dict = options[:dictionary]
         dict ||= {} if options.delete(:use_dictionary)
-        
+
         kopy = unless dict
-          super()
+          self.clone_without_deep
         else
           tableized_class = self.class.name.tableize.to_sym
           dict[tableized_class] ||= {}
-          dict[tableized_class][self] ||= super()
+          dict[tableized_class][self] ||= clone_without_deep
         end
 
         deep_exceptions = {}
@@ -106,29 +103,29 @@ class ActiveRecord::Base
           end
           deep_exceptions = exceptions.select{|e| e.kind_of?(Hash) }.inject({}){|m,h| m.merge(h) }
         end
-      
+
         if options[:include]
           Array(options[:include]).each do |association, deep_associations|
             if (association.kind_of? Hash)
               deep_associations = association[association.keys.first]
               association = association.keys.first
             end
-            
+
             opts = deep_associations.blank? ? {} : {:include => deep_associations}
             opts.merge!(:except => deep_exceptions[association]) if deep_exceptions[association]
             opts.merge!(:dictionary => dict) if dict
-          
+
             association_reflection = self.class.reflect_on_association(association)
             cloned_object = case association_reflection.macro
               when :belongs_to, :has_one
-                self.send(association) && self.send(association).clone(opts)
+                self.send(association) && self.send(association).clone_with_deep(opts)
               when :has_many, :has_and_belongs_to_many
-                reverse_association_name = association_reflection.klass.reflect_on_all_associations.detect do |a| 
+                reverse_association_name = association_reflection.klass.reflect_on_all_associations.detect do |a|
                   a.primary_key_name.to_s == association_reflection.primary_key_name.to_s
                 end.try(:name)
-                
-                self.send(association).collect do |obj| 
-                  tmp = obj.clone(opts)
+
+                self.send(association).collect do |obj|
+                  tmp = obj.clone_with_deep(opts)
                   tmp.send("#{reverse_association_name.to_s}=", kopy) if reverse_association_name
                   tmp.send("#{association_reflection.primary_key_name.to_s}=", nil)
                   tmp
@@ -140,8 +137,5 @@ class ActiveRecord::Base
 
         return kopy
       end
-    end
   end
-
-  include DeepCloneable
-end
+ActiveRecord::Base.send(:include, DeepCloneable)
